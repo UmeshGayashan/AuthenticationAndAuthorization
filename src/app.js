e5b227f2e36a5d768502802494523be7fdd12333
Umesh Gayashan <138327662+UmeshGayashan@users.noreply.github.com>
2024-06-09 13:03:31 +0530

    set Route in App.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 
+const signupRoute = require("./routes/signup");
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+app.use("/user",signupRoute);
 
 app.listen(PORT,() =>{
     console.log(`server is running on: http://localhost:${PORT}`);
